89456c94a3ada8ef9492326009b1c0ddc82a2636
xpextro93 <jonathanandrade@pursuit.org>
2020-07-04 14:59:27 -0400

    delete old code and redundant comments

--- Prim.js
+++ Prim.js
@@ -72,12 +72,3 @@ class Prim {
 
 
 export default Prim;
-
-
-// While there are walls in the list:
-// Pick a random wall from the list. If only one of the two cells that the wall divides is visited, then:
-// Make the wall a passage and mark the unvisited cell as part of the maze.
-// Add the neighboring walls of the cell to the wall list.
-// Remove the wall from the list.
-
-// http://weblog.jamisbuck.org/2011/1/10/maze-generation-prim-s-algorithm
