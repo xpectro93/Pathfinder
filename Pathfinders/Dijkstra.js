f27ddbdd6ba3eb2bda90ceb01428ac6ae16ebd22
xpextro93 <jonathanandrade@pursuit.org>
2020-07-09 17:31:50 -0400

    start adding code to dijkstras

--- Pathfinders/Dijkstra.js
+++ Pathfinders/Dijkstra.js
@@ -1,11 +1,11 @@
 class Dijkstra {
-    constructor(start, end, grid) {
+    constructor(start, target, grid) {
         this.start = start;
-        this.end = end;
+        this.target = target;
         this.grid = grid;
 
+        this.frontier = [start];
         this.visited = new Set();
-        this.toBeVisited = [];
     };
     getHeuristic(start, end) {
         let newX = Math.abs(start.x - end.x);
@@ -16,8 +16,31 @@ class Dijkstra {
 
     step () {
         //change start distance to to 0 before loop starts;
-        let current = this.openSet[0];
+        
+        //pick vertex with smallest distance 
+        let lowestDistanceIndex = 0;
+        for ( let i = 0; this.frontier.length; i++) {
+            if (this.frontier[lowestDistanceIndex].f > this.openSet[i].f) {
+                lowestDistanceIndex = i;
+            }
+        }
+        //remove from frontier
 
+        let current = this.frontier[lowestDistanceIndex];
+        
+        this.frontier = this.frontier.filter(vertex !== current);
+
+        if(current === this.target) {
+            console.log('Target has been found0');
+        }
+        
+
+        //for each neighbor of the one that got removed;
+        //tempDistance = distance of removed + distance(removed+ neighbor)
+
+        //if(tempDistnace < distance of neighbor)
+            //then update distance of neight to tempDistance
+            // neighbor/prev to removed
 
     }
 
