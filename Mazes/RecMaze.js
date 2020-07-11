10672b287897cfed455763578411d983378bf65f
xpextro93 <jonathanandrade@pursuit.org>
2020-07-11 17:54:20 -0400

    add randomNumber method to RecMaze file

--- Mazes/RecMaze.js
+++ Mazes/RecMaze.js
@@ -2,5 +2,11 @@ class RecMaze {
     constructor(grid) {
         this.grid = grid;
     }
+    randomNumber(min,max) {
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+    step() {
+        if(maxX - maxY < 3 || maxY - minY < 3) return;
+    }
 }
 export default RecMaze;
