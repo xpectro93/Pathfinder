ca5ec903ff55495d8510632c8c9391003df36b1e
xpextro93 <jonathanandrade@pursuit.org>
2020-07-12 16:55:30 -0400

    add first version for recMaze

--- Mazes/RecMaze.js
+++ Mazes/RecMaze.js
@@ -2,16 +2,42 @@ class RecMaze {
     constructor(grid) {
         this.grid = grid;
     }
+    avg(min,max) {
+      return Math.floor((min + max) / 2);
+    }
     randomNumber(min,max) {
-        return Math.floor(Math.random() * (max - min) + min);
+      return Math.floor(Math.random() * (max - min) + min);
     }
     step(minX,maxX,minY,maxY) {
+			const { grid, step } = this;
 
-        for(let i = minY; i < maxY;i++) {
-            for(let j = minX; j < maxX; j++) {
-              mtx[i][j] = 1
-            }
-        }
+			let lengthX =  maxX - minX;
+			let lengthY = maxY - minY;
+			
+			if(lengthX < 3 || lengthY < 3) return;
+
+			let midX = avg(minX, maxX);
+			let midY = avg(minY, minY);
+
+			
+			for(let y = minY; y < maxY; y++) {
+				for(let x = minX; x < maxX; x++) {
+					if(midX === x) grid[y][x].isWall = true;
+					else if(midY === y) grid[y][x].isWall = true;
+				}
+			}
+
+			let randX = this.randomNumber(minX,minY);
+			let randY = this.randomNumber(minY,maxY);
+			grid[randY][midX].isWall = false;
+			grid[midY][randX].isWall = false;
+
+			step(minX,midX,minY,midY);
+			step(midX,maxX,minY,midY);
+			step(minX, midX, midY,maxY);
+			step(midX,maxX, midY,maxY);
+			
+
     }
 }
 export default RecMaze;
